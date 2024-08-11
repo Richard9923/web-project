365c2d652328bc88d5ce8fa42c0695aab03f9990
Richard Rafael <programmingislife23@gmail.com>
2024-08-11 11:13:32 -0300

    criando router de links

--- web-project/src/components/Nav.tsx
+++ web-project/src/components/Nav.tsx
@@ -8,7 +8,7 @@ function Nav () {
     return (
         <Router>
         <ul id="nav">
-            <li className="nav"><a className="link" href="#">Home</a></li>
+            <li className="nav"><a className="link" href="/">Home</a></li>
             <li className="nav"><Link className="link" to="/sobre">Sobre</Link></li>
             <li className="nav"><a href="#" className="link">Contato</a></li>
             <li className="nav"><a href="#" className="link conhecimento">Conhecimento</a></li>
